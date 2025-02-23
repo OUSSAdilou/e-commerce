ddd7c85ff4b38af9488ebb609f67d9feae821732
OUSSAdilou <adilououssei@gmail.com>
2025-02-23 02:08:58 +0000

    mise à jours

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -8,44 +8,69 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class CartController extends AbstractController
+class CartController extends AbstractController
 {
 
     public function __construct(private readonly ProduitRepository $produitRepository){}
-
-    #[Route('/cart', name: 'app_cart', methods: ['GET'])]
+    #[Route('/cart', name: 'app_cart')]
     public function index(SessionInterface $session): Response
     {
         $cart = $session->get('cart', []);
         $cartWhitData = [];
-        foreach ($cart as $id => $quantity){
+        foreach($cart as $id => $quantity){
             $cartWhitData[] = [
                 'produit' => $this->produitRepository->find($id),
                 'quantity' => $quantity
             ];
         }
+
         $total = array_sum(array_map(function ($item){
             return $item['produit']->getPrix() * $item['quantity'];
         }, $cartWhitData));
+
+        // dd($cartWhitData);
         // dd($total);
         return $this->render('cart/index.html.twig', [
-            'items' => $cartWhitData,
+            'items'=>$cartWhitData,
             'total' => $total,
-            
+
         ]);
     }
 
-    #[Route('/cart/add/{id}/', name: 'app_cart_new', methods: ['GET'])]
+    #[Route('/cart/add/{id}', name: 'app_cart_new')]
     public function addToCart($id, SessionInterface $session): Response
     {
         $cart = $session->get('cart', []);
-        if (!empty($cart[$id])){
+        if(!empty($cart[$id]))
+        {
             $cart[$id]++;
         }else{
             $cart[$id] = 1;
+        }
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/remove/{id}', name: 'app_cart_produit_remove')]
+    public function remove($id, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+        if(!empty($cart[$id])){
+            unset($cart[$id]);
         }
         $session->set('cart', $cart);
 
         return $this->redirectToRoute('app_cart');
     }
+
+    #[Route('/cart/remove', name: 'app_cart_remove')]
+    public function removeAll(SessionInterface $session): Response
+    {
+        $session ->set('cart', []);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
 }
